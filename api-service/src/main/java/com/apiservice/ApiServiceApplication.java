7f6b1baa49e0433071176bbd4daa20dca8ff3132
RightHandedCoder <aniknishan@gmail.com>
2020-07-03 19:39:44 +0600

    Initial commit

---
+++ api-service/src/main/java/com/apiservice/ApiServiceApplication.java
@@ -0,0 +1,14 @@
+package com.apiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+public class ApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServiceApplication.class, args);
+    }
+
+}
---
+++ api-service/src/main/java/com/apiservice/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.apiservice.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class HomeController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "Hello world!";
+    }
+}
