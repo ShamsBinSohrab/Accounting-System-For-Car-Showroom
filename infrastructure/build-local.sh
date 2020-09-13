e192311f6d0b6ae94ef10131eee9cb6f29932220
shams <aniknishan@gmail.com>
2020-09-13 23:25:58 +0600

    added internal url for migration service

--- infrastructure/build-local.sh
+++ infrastructure/build-local.sh
@@ -3,7 +3,9 @@
 echo "Building docker images..."
 
 docker build -t "migration-service" -f ./backend/migration-service/Dockerfile ./backend/
-docker build -t "api-service" -f ./backend/api-service/Dockerfile ./backend/
+docker build -t "api-service" \
+  --build-arg context_path="/" \
+  -f ./backend/api-service/Dockerfile ./backend/
 
 echo "Deploying local docker swarm stack, updating images"
 docker stack deploy -c infrastructure/docker-compose.yml ascs
