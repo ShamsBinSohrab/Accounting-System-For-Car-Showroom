ba48b664420ecb0bf93245241e57f1f6c576adf2
RightHandedCoder <aniknishan@gmail.com>
2020-09-20 17:39:04 +0600

    code refactor

--- backend/api-service/src/main/java/com/apiservice/utils/configuration/JpaAuditConfiguration.java
+++ backend/api-service/src/main/java/com/apiservice/utils/configuration/JpaAuditConfiguration.java
@@ -2,13 +2,17 @@ package com.apiservice.utils.configuration;
 
 import com.apiservice.entity.master.operator.Operator;
 import com.apiservice.repository.operator.OperatorRepository;
+import com.apiservice.service.operator.OperatorService;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 @EnableJpaAuditing(auditorAwareRef = "auditor")
