15feee6214440ba83406cbc661c6e0ac0a078b03
RightHandedCoder <aniknishan@gmail.com>
2020-09-21 15:17:25 +0600

    fixed issue with jasper report not working in docker

--- backend/api-service/src/main/java/com/apiservice/service/ReportService.java
+++ backend/api-service/src/main/java/com/apiservice/service/ReportService.java
@@ -5,11 +5,12 @@ import com.apiservice.model.car.CarFilter;
 import com.apiservice.model.car.CarQueryBuilder;
 import com.apiservice.repository.car.CarRepository;
 import com.apiservice.utils.pagination.QueryBuilder;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -17,36 +18,38 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ReportService {
 
   private final CarRepository carRepository;
-  private String carReportTemplateSourcePath;
+  private Resource carReportTemplate;
 
   @PostConstruct
-  private void initReportTemplates() throws FileNotFoundException {
-    carReportTemplateSourcePath =
-        ResourceUtils.getFile("classpath:reports/car_report_template.jrxml").getAbsolutePath();
+  private void initReportTemplates() {
+    carReportTemplate = new ClassPathResource("reports/car_report_template.jrxml");
   }
 
   public byte[] getCarReport(CarFilter filter) {
     final QueryBuilder<Car> queryBuilder = new CarQueryBuilder(filter);
     final List<Car> cars = carRepository.findAll(queryBuilder.buildQuery());
-    return generatePdfReport(carReportTemplateSourcePath, cars);
+    return generatePdfReport(carReportTemplate, cars);
   }
 
-  private byte[] generatePdfReport(String sourceFilePath, Collection<?> collection) {
+  private byte[] generatePdfReport(Resource resource, Collection<?> collection) {
     try {
-      final JasperReport report = JasperCompileManager.compileReport(sourceFilePath);
+      final JasperReport report = JasperCompileManager.compileReport(resource.getInputStream());
       final JRBeanCollectionDataSource data = new JRBeanCollectionDataSource(collection);
       final JasperPrint print = JasperFillManager.fillReport(report, null, data);
       return JasperExportManager.exportReportToPdf(print);
-    } catch (JRException ex) {
-      throw new RuntimeException(ex);
+    } catch (JRException | IOException ex) {
+      log.info(ex.getMessage());
+      return new byte[0];
     }
   }
 }
