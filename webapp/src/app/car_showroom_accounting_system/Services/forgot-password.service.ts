a5d18e72443b412526b70be1cef73ae53d8623c4
Md. Hasan Uzzaman Noyon <noyon892@gmail.com>
2020-10-16 17:13:01 +0600

    Forgot Password Complete

--- webapp/src/app/car_showroom_accounting_system/Services/forgot-password.service.ts
+++ webapp/src/app/car_showroom_accounting_system/Services/forgot-password.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Common } from '../Common/common';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,9 @@ export class ForgotPasswordService {
   }
   public confirmResetPassword(data: any, token: any)
   {
-    const reqHeader = new HttpHeaders({'No-Auth': 'True', token});
+    const decodedPasswordToken = jwt_decode(token);
+    // console.log(decodedPasswordToken.sub);
+    const reqHeader = new HttpHeaders({'No-Auth': 'True', 'x-password-reset-token': decodedPasswordToken.sub});
     // reqHeader.set('token', token);
     return this.http.patch<any>(Common.url + '/confirmResetPassword', data, {headers: reqHeader});
   }
